09f570f1212b57130763c4a57f28df1b0627f564
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2018-11-03 17:59:41 +0500

    add button component from material UI in header section

---
+++ src/components/Header/Button.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  margin: {
+    margin: theme.spacing.unit
+  },
+  cssRoot: {
+    color: "#000000",
+    backgroundColor: "#FFFFFF",
+    "&:hover": {
+      backgroundColor: "#E8E8E8"
+    }
+  },
+  bootstrapRoot: {
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 16,
+    padding: "6px 12px",
+    border: "1px solid",
+    backgroundColor: "#007bff",
+    borderColor: "#007bff",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(","),
+    "&:hover": {
+      backgroundColor: "#0069d9",
+      borderColor: "#0062cc"
+    },
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#0062cc",
+      borderColor: "#005cbf"
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)"
+    }
+  }
+});
+
+function MyButton(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.container}>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classNames(classes.margin, classes.cssRoot)}
+        style={props.color}
+      >
+        Custom CSS
+      </Button>
+    </div>
+  );
+}
+
+MyButton.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(MyButton);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import Navebar from "./Navebar";
+import MyButton from "./Button";
 
+let whiteButton = {
+  color: "#0099FF",
+  backgroundColor: "#FFFFFF"
+};
+let blueButton = {
+  color: "white",
+  backgroundColor: "#1373d7"
+};
 export default class Header extends Component {
   render() {
     return (
       <div id="HeaderMain">
         <Navebar />
-        {/* <h1 className='text-center'>Automation for blockchains</h1> */}
+        <div className="mt-5">
+          <h1 className="text-center text-light pt-5">
+            Automation for blockchains
+          </h1>
+          <div className="mt-3">
+            <p className="text-center text-light">
+              Thinblock connects blockchain platforms with third-party services
+              <br />
+              to help build smart applications
+            </p>
+            <div className="row h-100 justify-content-center align-items-center">
+              <MyButton color={whiteButton} />
+              <MyButton color={blueButton} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

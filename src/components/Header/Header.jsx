23463719006252d38e79951f9f9440d94c67f519
ammy-bajwa <amirsadiqalisaifi@gmail.com>
2018-11-03 14:32:57 +0500

    render basic header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>This is Header</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;

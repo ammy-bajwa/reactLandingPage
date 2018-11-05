232b7b1af0e23ecb0fe927f903ee1bb25b43b724
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2018-11-05 18:56:30 +0500

    setting format

--- src/components/Triggers/Triggers.jsx
+++ src/components/Triggers/Triggers.jsx
@@ -6,11 +6,12 @@ export default class Triggers extends Component {
     return (
       <div id="triggerMain">
         <div id="contentTrigger">
-            <h1 className="row h-100 justify-content-center align-items-center m-5 text-light">
-              Use walletes or exchange API intrgration to automate execution
-            </h1><div className="m-5 row h-100 justify-content-center align-items-center">
-              <CodeCard />
-            </div>
+          <h1 className="row h-100 justify-content-center align-items-center m-5 text-light">
+            Use walletes or exchange API intrgration to automate execution
+          </h1>
+          <div className="m-5 row h-100 justify-content-center align-items-center">
+            <CodeCard />
+          </div>
         </div>
       </div>
     );

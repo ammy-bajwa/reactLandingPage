b5e0fb7940f95120142e38750d3d904abf79b6af
ammy-bajwa <amirsadiqalisaifi@gmail.com>
2018-11-05 14:52:13 +0500

    fixing deploy bug in server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../build')));
 app.listen(port, () => {
     console.log('server is up!')
 });
